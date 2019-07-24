def9b1c1896e144714ee21f8daa5399dd0516ed0
Nastia Krat <nastyakpi1995@gmail.com>
2019-07-24 10:13:11 +0300

    Updates

--- precache-manifest.65b1fd7b0ea9c0aad4f7a0f4a85baeb7.js
+++ precache-manifest.65b1fd7b0ea9c0aad4f7a0f4a85baeb7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "846a192574418b197737554b48685981",
+    "revision": "ed57c4a6f4992945b45275416e3e5ce3",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
+    "revision": "d5c8acd28738a31f9615",
     "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_todo-app/static/js/2.3310f33a.chunk.js"
+    "revision": "d3a51d49e2858388cfa3",
+    "url": "/react_todo-app/static/js/2.bf9c7b80.chunk.js"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
-    "url": "/react_todo-app/static/js/main.b6ea25b8.chunk.js"
+    "revision": "d5c8acd28738a31f9615",
+    "url": "/react_todo-app/static/js/main.747b9595.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
